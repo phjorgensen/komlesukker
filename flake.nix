a2cc6bc39919d6aa726ee423a5407cc66096d613
phjorgensen <phjorgensenn@gmail.com>
2025-08-14 19:30:53 +0200

    Format the flake.nix with nixfmt

--- flake.nix
+++ flake.nix
@@ -3,35 +3,38 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    # https://www.youtube.com/watch?v=Ss1IXtYnpsg
-    packages.${system}.default = pkgs.callPackage ./default.nix {};
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # https://www.youtube.com/watch?v=Ss1IXtYnpsg
+      packages.${system}.default = pkgs.callPackage ./default.nix { };
 
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        rust-analyzer
-        rustfmt
-        clippy
-      ];
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
 
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-      ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
 
-      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-      shellHook = ''
-        cargo -V
-        exec zsh
-      '';
+        shellHook = ''
+          cargo -V
+          exec zsh
+        '';
+      };
     };
-  };
 }
