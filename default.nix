716b50176852234a6ab6cc252d671ba4caf04ed4
phjorgensen <phjorgensenn@gmail.com>
2025-08-19 21:13:43 +0200

    Add webbrowser crate

--- default.nix
+++ default.nix
@@ -17,5 +17,5 @@ rustPlatform.buildRustPackage {
 
   # cargoLock.lockFile = ./Cargo.lock;
   # cargoHash = pkgs.lib.fakeHash;
-  cargoHash = "sha256-+9mUnm+7eC2/7sTf4dCV5geWpilovd0HelbWr4PSGoQ=";
+  cargoHash = "sha256-VLYSWKa/Np0c4/5IfBTJZDVtAr8Nt7DMDcEOOc0sLa8=";
 }
