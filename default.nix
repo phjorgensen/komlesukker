cd18f9279a835908ae5749b908a11df7f78b5ab9
phjorgensen <phjorgensenn@gmail.com>
2025-08-19 20:31:30 +0200

    Add clap crate

--- default.nix
+++ default.nix
@@ -17,5 +17,5 @@ rustPlatform.buildRustPackage {
 
   # cargoLock.lockFile = ./Cargo.lock;
   # cargoHash = pkgs.lib.fakeHash;
-  cargoHash = "sha256-U2u+9Dz8jqjeDMyO27uCAGxmv6mUkbpCNIDCUmKNqxs=";
+  cargoHash = "sha256-+9mUnm+7eC2/7sTf4dCV5geWpilovd0HelbWr4PSGoQ=";
 }
